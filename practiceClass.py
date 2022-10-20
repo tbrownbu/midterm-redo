e2fb50742ff6e725192da3aa51bd830862acf2f6
Todd <Todd_brown1@baylor.edu>
2022-10-19 20:26:11 -0500

    No output

--- practiceClass.py
+++ practiceClass.py
@@ -12,3 +12,38 @@ change the status attribute to False.
 2) a Booking class that has attributes for customer name and
 seat number. Create accessor methods for both attributes.'''
         
+class Play: 
+    def __init__(self, show_id, show_name, show_seats, show_date, show_status = True):
+        self.__show_id = show_id
+        self.__show_name = show_name
+        self.__show_seats = show_seats
+        self.__show_date = show_date
+        self.__show_status = show_status
+
+    
+    def get_name(self):
+        return self.__show_name
+
+    def get_seats(self):
+        return self.__show_seats
+
+    def get_status(self):
+        return self.__show_status
+
+    def num_seats_left(self):
+        self.__show_seats -=1
+
+    def set_status(self):
+        self.__show_status = False
+
+class Booking: 
+    def __init__(self,name,seat_number):
+        self.__name = name
+        self.__seat_number = seat_number
+        
+    def get_name(self):
+        return self.__name
+
+    def get_seat_number(self):
+        return self.__seat_number
+    
