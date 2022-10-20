90b2d8cabc614377a8f7c42aab586df4de90106e
Todd <Todd_brown1@baylor.edu>
2022-10-19 20:26:42 -0500

    idk

--- practiceProgram.py
+++ practiceProgram.py
@@ -1,4 +1,4 @@
-import practiceClassKEY as p
+import practiceClass as p
 import csv
 
 
@@ -46,11 +46,35 @@ error message as shown in output.JPG'''
 
 
 #open the csv file in read mode
+for rec in shows:
+    show_id = shows[rec]['id']
+    show_name = shows[rec]['name']
+    show_capacity = shows[rec]['capacity']
+    show_date = shows[rec]['event_date']
 
 
+    if show_id == 9587:
+        event = p.Play(show_id, show_name, show_capacity, show_date)
 #create a csv object from the file object from the step above
+infile = open('bookings.csv','r')
 
 
 
 # use a for loop to iterate through each record in the bookings file
+csvfile = csv.reader(infile, delimiter=',')
+next(csvfile)
 
+for record in csvfile:
+    if int(record[0]) == 9587:
+        if event.get_status():
+            x = p.Booking(record[1],record[2])
+            event.num_seats_left()
+            if event.get_seats()==0:
+                event.get_status() 
+        else:
+            print()
+            print('**********Error**********')
+            print('Guest Name: ',record[1])
+            print(event.get_name(), 'is sold out.')
+            print('*************************')
+            print()
